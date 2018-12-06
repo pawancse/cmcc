f093e5210a8b4e47e637ae957e8c3eaa05063dd6
Pawan  Kumar <757890@TCS.com>
2018-12-06 17:43:01 +0530

    Changes

--- routes/index.js
+++ routes/index.js
@@ -42,7 +42,7 @@ router.post('/login', function (req, res, next) {
   });
 });
 
-router.post('/register', function (req, res, next) {
+router.post('/changepassword', function (req, res, next) {
   var jsonList;
   var user = [];
   var email = [];
@@ -52,28 +52,17 @@ router.post('/register', function (req, res, next) {
     var alreadyUser = false;
     jsonList.user.forEach(function (item) {
       if (item.email == req.body.email) {
-        alreadyUser = true;
+        //user exist
+        if (item.password == req.body.epsw) {
+          item.password = req.body.npsw;
+        }
       }
-      var schema = {
-        name: item.name,
-        email: item.email
-      };
-      usersEmail.push(schema);
     })
-    if (alreadyUser == false) {
-      userSchema.name = req.body.name;
-      userSchema.email = req.body.email;
-      userSchema.password = req.body.psw;
-      jsonList.user.push(userSchema);
-      fs.writeFile(__dirname + "/user.json", JSON.stringify(jsonList), function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-        res.render('user', { usr: usersEmail });
-      });
-    }
-    else {
-      res.write('Email already Exist!! Please use different email');
-    }
+    fs.writeFile(__dirname + "/user.json", JSON.stringify(jsonList), function (err) {
+      if (err) throw err;
+      console.log('Password Changed!');
+      res.send('Your password is changed! <a href="/"> Click here to login</a>')
+    });
   });
 });
 
@@ -87,20 +76,24 @@ router.post('/status', function (req, res, next) {
       pass: 'Pa@9708677704'
     }
   });
+  var list = req.body.child.split("@@");
+  var email = list[0];
+  var name = list[1];
   var mailOptions = {
     from: 'pawan.kumar10@zoho.com', // sender address (who sends)
-    to: req.body.child,
-    bcc: 'pawan.bcet54@gmail.com', // list of receivers (who receives) req.body.child
+    to: email,
+    cc: ['pawan.bcet54@gmail.com','nishanmadhavan@gmail.com','nithya.greens@gmail.com','dineshramt@gmail.com','tharanyaece@gmail.com','jananisarathi@gmail.com','jeeviparthi1997@gmail.com','kiranasattigigeri@gmail.com','priyapradeep5617@gmail.com','dhasarathys@gmail.com','bhavyateja555@gmail.com'], // list of receivers (who receives) req.body.child
     subject: 'Task for Today', // Subject line
     //text: req.body.request.text, // plaintext body
-    html: 'Dear Child,<p> There is new task for you as below:</p><p>' + req.body.task + '</p> <p> Thanks <br/>Atlas GPO</p>' // html body
+    html: 'Dear '+ name +',<p> There is new task for you as below:</p><p>' + req.body.task + '</p> <p> Thanks <br/>Atlas GPO</p>' // html body
   };
+  console.log(mailOptions);
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
       console.log(error);
       res.send(error);
-    }else{
-     res.render('sendSuccess');
+    } else {
+      res.render('sendSuccess');
     }
   });
 });
